36e045698460690b47605f48a51b96df3c712211
Gotera <guntokun5@gmail.com>
2023-06-23 17:35:20 -0300

    .

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,17 @@
 import validatePassword from "../middlewares/validatePass.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import { users } from "../models/index.js";
+import error404 from '../errors/notFound.js'
 
 class UserController {
   static registerUser = async (req, res, next) => {
     try {
       const { name, password, email, confirmpassword } = req.body;
       const dataVerified = new users({
-        name: name,
-        email: email,
-        password: password,
+        name,
+        email,
+        password,
       });
       //CHECK IF PASSWORDS IS EQUAL
       if (password !== confirmpassword) {
@@ -17,14 +20,58 @@ class UserController {
       //CHECK IF USER ALREADY EXISTS
       const userExists = await users.findOne({ email: email });
       if (userExists) {
-        return res.status(422).json({ error: "Email já cadastrado! Por favor, utileze outro!" });
+        return res
+          .status(422)
+          .json({ error: "Email já cadastrado! Por favor, utileze outro!" });
       }
-
       await dataVerified.save();
       res.status(201).send(dataVerified.toJSON());
     } catch (err) {
       console.log(err);
       next(err);
+    }
+  };
+
+  static loginUser = async (req, res, next) => {
+    try {
+      //CHECK IF USER ALREADY EXIST
+      const { email, password } = req.body;
+      const user = await users.findOne({ email: email });
+      if (!user) {
+        return res.status(404).json({ error: "Usuário não encontrado!" });
+      }
+
+      //CHECK IF PASSWORD MATCH
+      const checkpassword = bcrypt.compare(password, user.password);
+      if (!checkpassword) {
+        return res.status(422).json({ error: "Senha Invalida!" });
+      }
+
+      // CREATE A TOKEN
+      const secret = process.env.SECRET;
+      const token = jwt.sign(
+        {
+          id: user.id,
+        },
+        secret
+      );
+
+      res
+        .status(200)
+        .json({ msg: "Autenticação realizada com sucesso!", token });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  //PRIVATE ROUTE
+  static porenquanto = async (req, res, next) => {
+    const id = req.params.id;
+    //CHECK IF USER ALREADY EXIST - PASSWORD
+    const user = await users.findById(id, "-password");
+
+    if (!user) {
+      next(error404)
     }
   };
 }
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import UserController from "../controllers/userController.js";
-import validatePassword from "../middlewares/validatePass.js";
 
 const router = express.Router();
 
-router.post("/api/register/user", UserController.registerUser);
+router
+  .post("/api/register/user", UserController.registerUser)
+  .post("/api/login", UserController.loginUser)
+  .get("/api/user:id", UserController.loginUser);
 export default router;
